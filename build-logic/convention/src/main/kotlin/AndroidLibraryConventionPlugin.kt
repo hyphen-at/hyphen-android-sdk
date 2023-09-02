645869479040b1a0cca1f8eeec8db0e51b63b16d
Dora Lee <dsa28s@naver.com>
2023-09-02 20:09:01 +0900

    build: add atomicfu plugin

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -15,6 +15,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("com.android.library")
                 apply("org.jetbrains.kotlin.android")
+                apply("kotlinx-atomicfu")
             }
 
             extensions.configure<LibraryExtension> {
@@ -28,6 +29,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             dependencies {
                 // "implementation"(libs.findLibrary("timber").get())
                 "implementation"(libs.findBundle("coroutines").get())
+                "implementation"(libs.findLibrary("atomicfu").get())
                 // "implementation"(libs.findLibrary("kotlin-immutable").get())
             }
         }
