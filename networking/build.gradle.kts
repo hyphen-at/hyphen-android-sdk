5483c3f778452d262cf0914b2e1f2dbdadbe5474
Dora Lee <dsa28s@naver.com>
2023-09-05 14:09:11 +0900

    chore: add networking module

---
+++ networking/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("hyphen.sdk.android.library")
+    id("hyphen.sdk.kotlin.serialization")
+}
+
+android {
+    namespace = "at.hyphen.android.sdk.networking"
+}
+
+dependencies {
+    implementation(projects.core)
+
+    implementation(libs.bundles.networking)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,3 +20,4 @@ dependencyResolutionManagement {
 rootProject.name = "hyphen-android-sdk"
 include(":sample")
 include(":core")
+include(":networking")
