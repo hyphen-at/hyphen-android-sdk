95bd41ccebf6632689ce2c2daa34d73ad837b29a
Dora Lee <dsa28s@naver.com>
2023-11-06 02:55:11 +0900

    feat(ui): remove dummy data in 2fa screen

--- ui/src/main/kotlin/at/hyphen/android/sdk/ui/twofactor/Hyphen2FAActivity.kt
+++ ui/src/main/kotlin/at/hyphen/android/sdk/ui/twofactor/Hyphen2FAActivity.kt
@@ -3,27 +3,31 @@ package at.hyphen.android.sdk.ui.twofactor
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
-import at.hyphen.android.sdk.core.common.HyphenOSName
-import at.hyphen.android.sdk.core.common.application.HyphenAppInformation
-import at.hyphen.android.sdk.core.common.device.HyphenDevice
-import at.hyphen.android.sdk.core.common.device.HyphenDeviceType
-import at.hyphen.android.sdk.core.common.twofactor.Hyphen2FARequest
-import at.hyphen.android.sdk.core.common.twofactor.Hyphen2FAStatus
-import at.hyphen.android.sdk.core.common.twofactor.Hyphen2FAStatusType
-import at.hyphen.android.sdk.core.common.twofactor.Hyphen2FAUserOpInfo
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.pointerInput
 import at.hyphen.android.sdk.flow.HyphenFlow
+import at.hyphen.android.sdk.flow.HyphenFlowCadence
 import at.hyphen.android.sdk.networking.HyphenNetworking
+import at.hyphen.android.sdk.networking.request.HyphenRequest2FAApprove
+import at.hyphen.android.sdk.ui.HyphenUI
 import at.hyphen.android.sdk.ui.theme.HyphenUITheme
 import kotlinx.coroutines.launch
+import kotlinx.serialization.json.Json
 import timber.log.Timber
 
 class Hyphen2FAActivity : AppCompatActivity() {
 
-    private var twoFactorAuth: Hyphen2FAStatus? by mutableStateOf(null)
+    private var isProcessing by mutableStateOf(false)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,81 +35,81 @@ class Hyphen2FAActivity : AppCompatActivity() {
         setContent {
             val coroutineScope = rememberCoroutineScope()
 
-            HyphenUITheme {
-                Hyphen2FAView(
-                    status = Hyphen2FAStatus(
-                        id = "0000000000000000",
-                        accountId = "000000000",
-                        request = Hyphen2FARequest(
-                            id = "000000",
-                            app = HyphenAppInformation(
-                                appId = "000000",
-                                appName = "Hyphen SDK Sample"
-                            ),
-                            userOpInfo = Hyphen2FAUserOpInfo(
-                                type = "sign-in",
-                                signIn = Hyphen2FAUserOpInfo.SignIn(
-                                    location = "Seoul, KR",
-                                    ip = "127.0.0.1",
-                                    email = "test@hyphen.at"
+            HyphenUI.pendingTwoFactorStatus?.let {
+                HyphenUITheme {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        Hyphen2FAView(
+                            status = it,
+                            onApproveButtonClick = {
+                                coroutineScope.launch {
+                                    approve2FA()
+                                }
+                            },
+                            onDenyButtonClick = {
+                                coroutineScope.launch {
+                                    reject2FA()
+                                }
+                            }
+                        )
+
+                        if (isProcessing) {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(
+                                        MaterialTheme.colorScheme.onBackground.copy(alpha = 0.2f)
+                                    )
+                                    .pointerInput(Unit) {}
+                            ) {
+                                CircularProgressIndicator(
+                                    modifier = Modifier.align(Alignment.Center),
                                 )
-                            ),
-                            srcDevice = HyphenDevice(
-                                id = "000000",
-                                publicKey = "000000",
-                                pushToken = "000000",
-                                name = "iPhone 15 Pro Max",
-                                osName = HyphenOSName.ANDROID,
-                                osVersion = "17.1.0",
-                                deviceManufacturer = "Apple",
-                                deviceModel = "iPhone16,2",
-                                lang = "KR",
-                                type = HyphenDeviceType.MOBILE,
-                            ),
-                            destDevice = HyphenDevice(
-                                id = "000000",
-                                publicKey = "000000",
-                                pushToken = "000000",
-                                name = "Google Pixel 6a",
-                                osName = HyphenOSName.ANDROID,
-                                osVersion = "12.0.1",
-                                deviceManufacturer = "Google",
-                                deviceModel = "pixel6a",
-                                lang = "United States",
-                                type = HyphenDeviceType.MOBILE,
-                            ),
-                            requestedAt = "2023-06-21T00:00:00:000",
-                            message = "<cadence script>",
-                        ),
-                        status = Hyphen2FAStatusType.PENDING,
-                        expiresAt = "2023-06-21T00:00:00:000",
-                    ),
-                    onApproveButtonClick = {
-                        Timber.tag("HyphenUI")
-                            .i("Generate and signing 'source device add public key' transaction...")
-                        coroutineScope.launch {
-                            HyphenFlow.signAndSendTransaction(
-                                cadenceScript = """
-                                    transaction {
-                                        execute {
-                                            log("Hello World")
-                                        }
-                                    }
-                                """.trimIndent(),
-                                arguments = emptyList()
-                            )
-                        }
-                    },
-                    onDenyButtonClick = {
-                        coroutineScope.launch {
-                            twoFactorAuth?.let {
-                                HyphenNetworking.Device.deny2FA(id = it.id)
-                                finish()
                             }
                         }
                     }
-                )
+                }
             }
         }
+    }
+
+    private suspend fun reject2FA() {
+        val twoFactorAuth = HyphenUI.pendingTwoFactorStatus
+            ?: error("HyphenUI SDK internal error. UI state 'twoFactorAuth' is nil.")
+
+        isProcessing = true
+
+        HyphenNetworking.Device.deny2FA(twoFactorAuth.id)
+        HyphenUI.pendingTwoFactorStatus = null
+        finish()
+    }
+
+    private suspend fun approve2FA() {
+        isProcessing = true
+
+        Timber.tag("HyphenUI")
+            .i("Generate and signing 'source device add public key' transaction...")
+
+        val twoFactorAuth = HyphenUI.pendingTwoFactorStatus
+            ?: error("HyphenUI SDK internal error. UI state 'twoFactorAuth' is nil.")
+
+        val json = Json {
+            prettyPrint = true
+            ignoreUnknownKeys = true
+            explicitNulls = false
+        }
+        val cadenceScript: HyphenFlowCadence = json.decodeFromString(twoFactorAuth.request.message)
+        val tx = HyphenFlow.signAndSendTransaction(cadenceScript.cadence, arguments = emptyList())
+
+        HyphenNetworking.Device.approve2FA(
+            id = twoFactorAuth.id,
+            payload = HyphenRequest2FAApprove(txId = tx)
+        )
+
+        HyphenUI.pendingTwoFactorStatus = null
+        Timber.tag("HyphenUI").i("Approve 2FA done!")
+
+        finish()
     }
 }
