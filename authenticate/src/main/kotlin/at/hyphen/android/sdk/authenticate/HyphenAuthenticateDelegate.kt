92e0d1af95e788e9fcf59565ce0555a3becba4cd
Dora Lee <dsa28s@naver.com>
2023-11-05 09:40:17 +0900

    refactor(auth): change ActivityResult signature in AuthenticateDelegate

--- authenticate/src/main/kotlin/at/hyphen/android/sdk/authenticate/HyphenAuthenticateDelegate.kt
+++ authenticate/src/main/kotlin/at/hyphen/android/sdk/authenticate/HyphenAuthenticateDelegate.kt
@@ -1,12 +1,12 @@
 package at.hyphen.android.sdk.authenticate
 
-import android.app.Instrumentation
 import android.content.Intent
+import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 
 interface HyphenAuthenticateDelegate {
 
     val hyphenAuthenticateActivityResultLauncher: ActivityResultLauncher<Intent>
 
-    var hyphenActivityResultCallback: ((Instrumentation.ActivityResult) -> Unit)?
+    var hyphenActivityResultCallback: ((ActivityResult) -> Unit)?
 }
